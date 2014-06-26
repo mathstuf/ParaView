5eb6c0d35e17c1ac5a32474ff33893fe4ebeccba
Ben Boeckel <ben.boeckel@kitware.com>
2014-06-26 14:52:33 -0400

    numpy: support a static numpy
    Change-Id: I43430dde5f325e2b0b20a76b99156c4c47d1801a

--- CommandLineExecutables/pvpython.cxx
+++ CommandLineExecutables/pvpython.cxx
@@ -17,10 +17,37 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkPVConfig.h" // Required to get build options for paraview
 #ifndef BUILD_SHARED_LIBS
 #include "pvStaticPluginsInit.h"
+#include "vtkPython.h"
 #endif
+
+extern "C" void initmultiarray(void);
+extern "C" void initscalarmath(void);
+extern "C" void initumath(void);
+extern "C" void init_dotblas(void);
+extern "C" void init_sort(void);
+extern "C" void initfftpack_lite(void);
+extern "C" void init_compiled_base(void);
+extern "C" void initlapack_lite(void);
+extern "C" void init_capi(void);
+extern "C" void initmtrand(void);
+
+static void paraview_import_static_modules()
+{
+  PyImport_AppendInittab("numpy.core.multiarray", initmultiarray);
+  PyImport_AppendInittab("numpy.core.scalarmath", initscalarmath);
+  PyImport_AppendInittab("numpy.core.umath", initumath);
+  PyImport_AppendInittab("numpy.core._dotblas", init_dotblas);
+  PyImport_AppendInittab("numpy.core._sort", init_sort);
+  PyImport_AppendInittab("numpy.fft.fftpack_lite", initfftpack_lite);
+  PyImport_AppendInittab("numpy.lib._compiled_base", init_compiled_base);
+  PyImport_AppendInittab("numpy.linalg.lapack_lite", initlapack_lite);
+  PyImport_AppendInittab("numpy.numarray._capi", init_capi);
+  PyImport_AppendInittab("numpy.random.mtrand", initmtrand);
+}
 
 int main(int argc, char* argv[])
 {
+  paraview_import_static_modules();
 #ifndef BUILD_SHARED_LIBS
   paraview_static_plugins_init();
 #endif
