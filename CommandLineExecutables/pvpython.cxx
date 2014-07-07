93186031dc6e90647550bb6957c18c82a1783ab1
Ben Boeckel <ben.boeckel@kitware.com>
2014-07-07 15:42:56 -0400

    static numpy: add support to pvbatch as well
    Change-Id: Ic49115dea59fa27abcbf0588d6a104d5382d5e27

--- CommandLineExecutables/pvpython.cxx
+++ CommandLineExecutables/pvpython.cxx
@@ -17,44 +17,15 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkPVConfig.h" // Required to get build options for paraview
 #ifndef BUILD_SHARED_LIBS
 #include "pvStaticPluginsInit.h"
-#include "vtkPython.h"
 #endif
-
 #ifdef HAVE_NUMPY_STATIC
-extern "C" void initmultiarray(void);
-extern "C" void initscalarmath(void);
-extern "C" void initumath(void);
-#ifdef HAVE_NUMPY_BLAS_LIB
-extern "C" void init_dotblas(void);
-#endif
-extern "C" void init_sort(void);
-extern "C" void initfftpack_lite(void);
-extern "C" void init_compiled_base(void);
-extern "C" void initlapack_lite(void);
-extern "C" void init_capi(void);
-extern "C" void initmtrand(void);
-
-static void paraview_import_static_modules()
-{
-  PyImport_AppendInittab("numpy.core.multiarray", initmultiarray);
-  PyImport_AppendInittab("numpy.core.scalarmath", initscalarmath);
-  PyImport_AppendInittab("numpy.core.umath", initumath);
-#ifdef HAVE_NUMPY_BLAS_LIB
-  PyImport_AppendInittab("numpy.core._dotblas", init_dotblas);
-#endif
-  PyImport_AppendInittab("numpy.core._sort", init_sort);
-  PyImport_AppendInittab("numpy.fft.fftpack_lite", initfftpack_lite);
-  PyImport_AppendInittab("numpy.lib._compiled_base", init_compiled_base);
-  PyImport_AppendInittab("numpy.linalg.lapack_lite", initlapack_lite);
-  PyImport_AppendInittab("numpy.numarray._capi", init_capi);
-  PyImport_AppendInittab("numpy.random.mtrand", initmtrand);
-}
+#include "pvnumpy-static.h"
 #endif
 
 int main(int argc, char* argv[])
 {
 #ifdef HAVE_NUMPY_STATIC
-  paraview_import_static_modules();
+  paraview_numpy_static_setup();
 #endif
 #ifndef BUILD_SHARED_LIBS
   paraview_static_plugins_init();
